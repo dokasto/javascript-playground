34fba62868b1d81674c2669a29bafc748e5e0baf
Udo <writeudo@gmail.com>
2016-01-14 09:36:59 +0100

    implemented radix sorting

---
+++ data-structures/queue.js
@@ -0,0 +1,111 @@
+/**
+ * Queue Implementation
+ */
+var Queue = function() {
+  this.dataStore = [];
+};
+
+/**
+ * Add an element to the end of the queue
+ */
+Queue.prototype.enqueue = function(element) {
+  this.dataStore.push(element);
+};
+
+/**
+ * Remove an element from the front of the queue
+ */
+Queue.prototype.dequeue = function() {
+  return this.dataStore.shift();
+};
+
+
+Queue.prototype.front = function() {
+  return this.dataStore[0];
+};
+
+Queue.prototype.back = function() {
+  return this.dataStore[this.dataStore.length - 1];
+};
+
+Queue.prototype.toString = function() {
+  var result = "";
+  this.dataStore.forEach(function(item) {
+    result += item + "\n";
+  });
+  return result;
+};
+
+Queue.prototype.empty = function() {
+  if (this.dataStore.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ * RadixSort Class
+ * @param {Array} nums   an array of numbers
+ * @param {Array} queues an array of Queue objects
+ */
+var RadixSort = function(nums, queues) {
+  this.nums = nums;
+  this.queues = queues;
+};
+
+/**
+ * Distribute numbers according by the place (1s, 10s)
+ * @param  {Array} nums  [array of numbers]
+ * @param  {Array} queues [array of queues]
+ * @param  {int} n      [description]
+ * @param  {int} digit  [represents it's 1s or 10s place]
+ * @return {null}
+ */
+RadixSort.prototype.distribute = function(n, digit) {
+  for (var i = 0; i < n; i++) {
+    if (digit == 1) {
+      this.queues[this.nums[i] % 10].enqueue(this.nums[i]);
+    } else if (digit == 10) {
+      this.queues[Math.floor(this.nums[i] / 10)].enqueue(this.nums[i]);
+    }
+  }
+};
+
+/**
+ * Collect all sorted numbers
+ * @param  {Array} queues
+ * @param  {Array} nums
+ * @return {null}
+ */
+RadixSort.prototype.collect = function() {
+  var i = 0;
+  for (var digit = 0; digit < 10; digit++) {
+    while (!this.queues[digit].empty()) {
+      this.nums[i] = this.queues[digit].dequeue();
+      i++;
+    }
+  }
+};
+
+RadixSort.prototype.display = function() {
+  console.log(this.nums.join());
+};
+
+/**
+ * Test Radix sort
+ */
+var queues = [];
+for (var i = 0; i < 10; i++) {
+  queues[i] = new Queue();
+}
+var nums = [45, 72, 93, 51, 21, 16, 70, 41, 27, 31];
+
+var radix = new RadixSort(nums, queues);
+
+console.log("Before radix sort: ");
+radix.display();
+radix.distribute(10, 10);
+radix.collect();
+console.log("\n\nAfter radix sort: ");
+radix.display();
