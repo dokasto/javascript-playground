44c5834fcafce43d9a7d4d1b9ebc2746185980f4
Udo <writeudo@gmail.com>
2016-01-15 08:22:37 +0100

    implemented double linked list

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -78,3 +78,73 @@ LList.prototype.display = function() {
     currNode = currNode.next;
   }
 };
+
+
+
+/**
+ * Double Linked List Implementation
+ * Inherit from LList
+ */
+var DLList = function() {
+  // extend LinkedLikst class
+  LList.call(this);
+};
+
+/**
+ * Create a DoubleLinkedList prototype that inherits
+ * from linkedlist prototyp
+ */
+DLList.prototype = Object.create(LList.prototype);
+
+/* set the constructor property to refer to double linked list */
+DLList.prototype.constructor = DLList;
+
+/**
+ * Modify internal Node class
+ */
+DLList.prototype.node = function(element) {
+  this.element = element;
+  this.next = null;
+  this.previous = null;
+};
+
+
+DLList.prototype.insert = function() {
+  var newElement = arguments[0];
+  var item = arguments[1] || "head";
+  var newNode = new this.Node(newElement);
+  var current = this.find(item);
+  newNode.next = current.next;
+  newNode.previous = current;
+  current.next = newNode;
+  return true;
+};
+
+DLList.prototype.remove = function(item) {
+  var currNode = this.find(item);
+  if (currNode.next != null) {
+    currNode.previous.next = currNode.next;
+    currNode.next.previous = currNode.previous;
+    currNode.next = null;
+    currNode.previous = null;
+    return true;
+  }
+};
+
+DLList.prototype.findLast = function() {
+  var currNode = this.head;
+  while (currNode.next != null) {
+    currNode = currNode.next;
+  }
+  return currNode;
+};
+
+DLList.prototype.displayReverse = function() {
+  var currNode = this.head;
+  currNode = this.findLast();
+  console.log("Display Nodes in reverse");
+  while (currNode.previous != null) {
+    console.log(currNode.element);
+    currNode = currNode.previous;
+  }
+};
