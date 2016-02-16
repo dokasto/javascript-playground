92f66c50dc6a15aa5c7348d8193a37898d3ab89a
Udo <writeudo@gmail.com>
2016-02-16 04:59:25 +0100

    skipping graph search, to be continued

--- data_structures/graphs.js
+++ data_structures/graphs.js
@@ -72,9 +72,12 @@ Graph.prototype.dfs = function(v) {
 /**
  * Tests
  */
-var g = new Graph(5);
-g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(1, 3);
-g.addEdge(2, 4);
-g.show();
+(function() {
+  var g = new Graph(5);
+  g.addEdge(0, 1);
+  g.addEdge(0, 2);
+  g.addEdge(1, 3);
+  g.addEdge(2, 4);
+  g.show();
+  g.dfs(0);
+})();
