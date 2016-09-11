92898f25d5acd087a3b16947a75012dc6f429bee
Udo <writeudo@gmail.com>
2016-09-11 15:41:41 +0100

    added function to build table

--- dom/traverse.js
+++ dom/traverse.js
@@ -24,3 +24,32 @@ var listTextNodes = function(node) {
   traverse(node);
   return textNodes;
 };
+
+/**
+ * Build a table
+ * @param  {int} rows
+ * @param  {int} cols
+ * @return {DOM Node}
+ */
+var buildTable = function(rows, cols) {
+
+  let td = function(text) {
+    let col = document.createElement('td');
+    let txtNode = document.createTextNode(text);
+    col.appendChild(txtNode);
+    return col;
+  };
+
+  let table = document.createElement('table');
+
+  for (let i = 0; i < rows; i++) {
+    let row = document.createElement('tr');
+    for (let j = 0; j < cols; j++) {
+      let col = td(`${i} ${j}`);
+      row.appendChild(col);
+    }
+    table.appendChild(row);
+  }
+
+  return table;
+};
