b9fb5f17128af68a320575cedd2c17c4b24791ce
Udo <writeudo@gmail.com>
2016-09-30 07:30:50 +0100

    added Javascript concepts folder and p inheritance file

---
+++ javascript_concepts/inheritance.js
@@ -0,0 +1,60 @@
+/**
+ * This shows the different type of inheritance in Javascript
+ * - Classical inheritance
+ * - Prototypical inheritance [Recommended]
+ *
+ * Favor object composition over inheritance - Erric Elliot
+ */
+
+
+/**
+ * Classical inheritance
+ */
+
+function Animal(name) {
+  this.name = name;
+}
+
+Animal.prototype.getName = function() {
+  return this.name;
+};
+
+function Dog() {
+  Animal.apply(this, arguments);
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+
+Dog.prototype.constructor = Dog;
+
+/***** is this ☝ just crazy :( ***/
+
+
+/**
+ * Prototypical inheritance
+ */
+
+const Vehicle = {
+  create: function(name) {
+    var self = this;
+    self.name = name;
+    return self;
+  },
+  getName: function() {
+    return this.name;
+  }
+};
+
+const Bus = Object.create(Vehicle);
+
+Bus.create = function(name) {
+  return Vehicle.create.call(this, name);
+};
+
+/**
+ * usage
+ */
+let saloonCar = Bus.create('Honda');
+// saloonCar.getName();
+
+/***** better right ☝ I know :) ***/
