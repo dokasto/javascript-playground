8124e418b8ed98402c9f8848dc69b211ed8b796a
Udo <writeudo@gmail.com>
2017-09-21 23:03:56 +0200

    added fibonacci

--- Algorithms/random.js
+++ Algorithms/random.js
@@ -27,3 +27,45 @@ function tripleSum(sum, numbers) {
  * @param {int} n 
  */
 const factorial = n => (n < 2 ? 1 : n * factorial(n - 1));
+
+const isPrime = n => {
+	let multiples = 0;
+	for (let i = 0; i <= n; i++) {
+		if (n % i === 0) multiples++;
+	}
+	return multiples === 2;
+};
+
+const fib = n => {
+	if (n <= 0) return 0;
+	if (n === 1) return 1;
+	return fib(n - 1) + fib(n - 2);
+};
+
+const isSorted = arr => {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > arr[i + 1]) return false;
+	}
+	return true;
+};
+
+const filter = () => {};
+
+describe('Random algorithms', function() {
+	it(`3 is a prime number`, function() {
+		expect(isPrime(3)).toEqual(true);
+	});
+
+	it('5 Factorial is 120', () => {
+		expect(factorial(5)).toEqual(120);
+	});
+
+	it('Fibonacci of 10 is 55', () => {
+		expect(fib(10)).toEqual(55);
+	});
+
+	it('Array is sorted', () => {
+		expect(isSorted([3, 9, -3, 10])).toEqual(false);
+		expect(isSorted([[-5, 0, 3, 9]])).toEqual(true);
+	});
+});
