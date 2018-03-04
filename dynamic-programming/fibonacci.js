c590d77381f8acb3215033c0d290ff110cbc7100
Udo <writeudo@gmail.com>
2018-03-04 12:10:30 +0100

    add optimized fibonacci

--- dynamic-programming/fibonacci.js
+++ dynamic-programming/fibonacci.js
@@ -3,3 +3,12 @@
  */
 
  const fib = n => n < 2 ? n : fib(n-1) + fib(n-2);
+
+ // optimized fibonacci
+const optimizedFib = n => {
+	const cache = {};
+	if (n < 2) return n;
+	if (cache[n]) return cache[n];
+	cache[n] = fib(n-1) + fib(n-2);
+	return cache[n];
+};
