16c6f415dba1dfcc32ea553998193633540350ab
Udo <writeudo@gmail.com>
2018-03-17 09:59:30 +0100

    update quicksort

--- algorithms/sorting/quick.js
+++ algorithms/sorting/quick.js
@@ -1,22 +1,17 @@
-const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
-
-
-const quickSort = arr => {
-  if (arr.length < 2) return arr;
+const quickSort = list => {
   
-  const pivot = arr[0];
+  const pivot = list[0];
   const lesser = [];
   const greater = [];
   
-  for(let i = 0; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      lesser.push(arr[i]);
-    } else if (arr[i] > pivot) {
-      greater.push(arr[i]);
+  for(let i=0; i<list.length; i++) {
+    if(list[i] > pivot) {
+      greater.push(list[i]);
+    }
+    if(list[i] < pivot) {
+      lesser.push(list[i]);
     }
   }
   
   return quickSort(lesser).concat(pivot, quickSort(greater));
 };
-
-console.log(quickSort(array));
